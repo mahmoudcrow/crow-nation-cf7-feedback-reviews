aeaaaad13dcea5fecd883bdda41042f6d7e442a8
Mahmoud Mostafa <48212598+mahmoudcrow@users.noreply.github.com>
2026-01-13 15:00:22 +0400

    V1.0.0 Beta 13

--- includes/class-cf7fr-settings.php
+++ includes/class-cf7fr-settings.php
@@ -10,19 +10,6 @@ class CF7FR_Settings
     {
         add_action('admin_menu', [__CLASS__, 'register_settings_page']);
     }
-
-    public static function register_settings_page()
-    {
-        add_submenu_page(
-            'cf7fr-reviews',
-            'CF7 Reviews Settings',
-            'Settings',
-            'manage_options',
-            'cf7fr-settings',
-            [__CLASS__, 'render_settings']
-        );
-    }
-
     public static function render_settings()
     {
         echo '<div class="wrap"><h1>Settings</h1>';
